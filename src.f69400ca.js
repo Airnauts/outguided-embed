5c69db1df8937c5dd4024fe7e87aae07084a96d7
Mateusz Paulski <mateusz@c1.com.pl>
2022-07-14 09:34:35 +0200

    Updates

--- src.f69400ca.js
+++ src.f69400ca.js
@@ -2876,201 +2876,19 @@ exports.Snippet = void 0;
 
 var _preact = require("preact");
 
-var _hooks = require("preact/hooks");
-
-var __awaiter = void 0 && (void 0).__awaiter || function (thisArg, _arguments, P, generator) {
-  function adopt(value) {
-    return value instanceof P ? value : new P(function (resolve) {
-      resolve(value);
-    });
-  }
-
-  return new (P || (P = Promise))(function (resolve, reject) {
-    function fulfilled(value) {
-      try {
-        step(generator.next(value));
-      } catch (e) {
-        reject(e);
-      }
-    }
-
-    function rejected(value) {
-      try {
-        step(generator["throw"](value));
-      } catch (e) {
-        reject(e);
-      }
-    }
-
-    function step(result) {
-      result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected);
-    }
-
-    step((generator = generator.apply(thisArg, _arguments || [])).next());
-  });
-};
-
-var __generator = void 0 && (void 0).__generator || function (thisArg, body) {
-  var _ = {
-    label: 0,
-    sent: function sent() {
-      if (t[0] & 1) throw t[1];
-      return t[1];
-    },
-    trys: [],
-    ops: []
-  },
-      f,
-      y,
-      t,
-      g;
-  return g = {
-    next: verb(0),
-    "throw": verb(1),
-    "return": verb(2)
-  }, typeof Symbol === "function" && (g[Symbol.iterator] = function () {
-    return this;
-  }), g;
-
-  function verb(n) {
-    return function (v) {
-      return step([n, v]);
-    };
-  }
-
-  function step(op) {
-    if (f) throw new TypeError("Generator is already executing.");
-
-    while (_) {
-      try {
-        if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-        if (y = 0, t) op = [op[0] & 2, t.value];
-
-        switch (op[0]) {
-          case 0:
-          case 1:
-            t = op;
-            break;
-
-          case 4:
-            _.label++;
-            return {
-              value: op[1],
-              done: false
-            };
-
-          case 5:
-            _.label++;
-            y = op[1];
-            op = [0];
-            continue;
-
-          case 7:
-            op = _.ops.pop();
-
-            _.trys.pop();
-
-            continue;
-
-          default:
-            if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) {
-              _ = 0;
-              continue;
-            }
-
-            if (op[0] === 3 && (!t || op[1] > t[0] && op[1] < t[3])) {
-              _.label = op[1];
-              break;
-            }
-
-            if (op[0] === 6 && _.label < t[1]) {
-              _.label = t[1];
-              t = op;
-              break;
-            }
-
-            if (t && _.label < t[2]) {
-              _.label = t[2];
-
-              _.ops.push(op);
-
-              break;
-            }
-
-            if (t[2]) _.ops.pop();
-
-            _.trys.pop();
-
-            continue;
-        }
-
-        op = body.call(thisArg, _);
-      } catch (e) {
-        op = [6, e];
-        y = 0;
-      } finally {
-        f = t = 0;
-      }
-    }
-
-    if (op[0] & 5) throw op[1];
-    return {
-      value: op[0] ? op[1] : void 0,
-      done: true
-    };
-  }
-};
-
 var Snippet = function Snippet(_a) {
   var code = _a.code;
-  var textAreaRef = (0, _hooks.useRef)();
-
-  var _b = (0, _hooks.useState)(false),
-      focused = _b[0],
-      setFocused = _b[1];
-
-  var onFocus = function onFocus(e) {
-    return __awaiter(void 0, void 0, void 0, function () {
-      var target;
-      return __generator(this, function (_a) {
-        target = e.target;
-        setFocused(true);
-        return [2
-        /*return*/
-        ];
-      });
-    });
-  };
-
-  (0, _hooks.useEffect)(function () {
-    var _a;
-
-    if (focused) {
-      (_a = textAreaRef.current) === null || _a === void 0 ? void 0 : _a.blur();
-      setFocused(false);
-    }
-  }, [setFocused, focused]);
-  return (0, _preact.h)("div", {
-    class: "snippet-preview".concat(focused ? ' snippet-preview--focused' : '')
-  }, (0, _preact.h)("div", {
-    class: "snippet-preview__info"
-  }, "Copied!"), (0, _preact.h)("textarea", {
-    readonly: true,
-    class: "snippet-preview__textarea",
-    ref: function ref(_ref) {
-      return textAreaRef.current = _ref;
-    },
-    onFocus: onFocus,
+  return (0, _preact.h)("textarea", {
     cols: 80,
     rows: 7,
     dangerouslySetInnerHTML: {
       __html: code
     }
-  }));
+  });
 };
 
 exports.Snippet = Snippet;
-},{"preact":"../node_modules/preact/dist/preact.module.js","preact/hooks":"../node_modules/preact/hooks/dist/hooks.module.js"}],"pages/TripWidget.tsx":[function(require,module,exports) {
+},{"preact":"../node_modules/preact/dist/preact.module.js"}],"pages/TripWidget.tsx":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -5360,7 +5178,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58260" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59468" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
